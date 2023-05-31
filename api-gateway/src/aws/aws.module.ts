7b7026b8f5461049d3f909c5dba41b748f673c41
IgorSilvaZZ <igor06silva12@gmail.com>
2023-05-30 22:23:57 -0300

    Initial config s3 instance

--- api-gateway/src/aws/aws.module.ts
+++ api-gateway/src/aws/aws.module.ts
@@ -1,3 +1,5 @@
+/* eslint-disable prettier/prettier */
+
 import { Module } from '@nestjs/common';
 import { AwsService } from './aws.service';
 
--- api-gateway/src/aws/aws.service.ts
+++ api-gateway/src/aws/aws.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
 
+import { Injectable, Logger } from '@nestjs/common';
+import * as AWS from 'aws-sdk';
 @Injectable()
-export class AwsService {}
+export class AwsService {
+  private logger = new Logger(AwsService.name);
+
+  public async uploadArquivo(file: any, id: string) {
+    const s3Instance = new AWS.S3({
+      endpoint: 'http://localhost:4000',
+      accessKeyId: 'S3RVER',
+      secretAccessKey: 'S3RVER',
+    });
+
+    const [, fileExtension] = file.originalname.split('.');
+
+    const urlKey = `${id}.${fileExtension}`;
+
+    this.logger.log(`urlKey: ${urlKey}`);
+
+    const params = {
+      Body: file.buffer,
+      Bucket: 'smartranking',
+      Key: urlKey,
+    };
+
+    const data = s3Instance
+      .putObject(params)
+      .promise()
+      .then((data) => data);
+
+    return data;
+  }
+}
